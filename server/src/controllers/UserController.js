bb4b5d65b2ff6f0beb1b6e21589a22cff6c0311b
bernborgess <bernborgess@outlook.com>
2023-08-09 14:38:10 -0300

    tested middlewares

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -1,15 +1,15 @@
 class UserController {
-  /**
-    * index - GET
-    * show - GET specific
-    * create - POST creates register
-    * update - PUT
-    * delete - DELETE removes a register
-    */
-  create(req, res) {
-    const { name, email, password } = req.body;
-    res.json({ name, email, password });
-  }
+    /**
+  * index - GET
+  * show - GET specific
+  * create - POST creates register
+  * update - PUT
+  * delete - DELETE removes a register
+  */
+    create(req, res) {
+        const { name, email, password } = req.body;
+        res.status(201).json({ name, email, password });
+    }
 
 }
 
