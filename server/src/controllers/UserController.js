165ecc997ffa62bf617a70b0dc70a454067f9273
bernborgess <bernborgess@outlook.com>
2023-08-10 11:41:05 -0300

    update name and email

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -11,8 +11,8 @@ class UserController {
     * update - PUT
     * delete - DELETE removes a register
     */
-    async create(req, res) {
-        const { name, email, password } = req.body;
+    async create(request, response) {
+        const { name, email, password } = request.body;
         if (!name) {
             throw new AppError("Nome é obrigatório");
         }
@@ -32,7 +32,39 @@ class UserController {
             "INSERT INTO users (name,email,password) VALUES (?,?,?)",
             [name, email, hashedPassword]);
 
-        res.status(201).json();
+        response.status(201).json();
+    }
+
+    async update(request, response) {
+        const { name, email } = request.body;
+        const { id } = request.params;
+
+        const database = await sqliteConnection();
+        const user = await database.get(
+            "SELECT * FROM users WHERE id = (?)", [id]);
+
+        if (!user) {
+            throw new AppError("Usuário não encontrado.");
+        }
+
+        const userWithUpdatedEmail = await database.get(
+            "SELECT * FROM users WHERE email = (?)", [email]);
+
+        if (userWithUpdatedEmail &&
+            userWithUpdatedEmail.id !== user.id) {
+            throw new AppError(`Este email já está em uso id=${id},${userWithUpdatedEmail.id}`);
+        }
+
+        await database.run(`--sql
+            UPDATE users SET
+                name = ?,
+                email = ?,
+                updated_at = ?
+            WHERE id = ?` , [
+            name, email, new Date(), id
+        ]);
+
+        return response.json();
     }
 
 }
